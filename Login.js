6eb7d8e741422481ffb9e0803e3ef729c1e14a33
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-02-12 17:05:35 +0100

    feat: add `Login.js`

---
+++ Login.js
@@ -0,0 +1,29 @@
+const Login = async (name, password) => {
+  if (name == "John Doe" && password == "testing") {
+    return {
+      status: true,
+      code: 200,
+      data: {
+        status: true,
+        message: {
+          token: "testToken",
+          Description: `${name} has been logged in successfully`,
+        },
+      },
+    };
+  } else {
+    return {
+      status: false,
+      code: 400,
+      data: {
+        status: false,
+        message: {
+          token: null,
+          Description: `Something went wrong`,
+        },
+      },
+    };
+  }
+};
+
+module.exports = { Login };
